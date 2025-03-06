a54f850fa0291c04153244fecb5b3280f3086a1c
MasedMSD <68379695+MasedMSD@users.noreply.github.com>
2025-03-06 14:23:19 +0500

    little fixes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,5 @@
-import { config } from "dotenv";
-import { resolve } from "path";
-import SteamTotp from "steam-totp";
+import SteamTOTP from "steam-totp";
 import SteamUser from "steam-user";
-
-const user = new SteamUser({ language: "russian", autoRelogin: true });
-config({ path: resolve(process.cwd(), ".env") });
 
 /**
  * @type {Array<number>}
@@ -12,25 +7,24 @@ config({ path: resolve(process.cwd(), ".env") });
  *
  * @example [2881650, 1422450]
  */
-const games: Array<number> = require("./games.json");
+import games from "./games.json" with { type: "json" };
 
-(async () => {
-	user.logOn({
-		accountName: process.env.ACCOUNT_NAME,
-		password: process.env.PASSWORD,
-		rememberPassword: true,
-		twoFactorCode: process.env.SHARED_SECRET
-			? SteamTotp.getAuthCode(process.env.SHARED_SECRET)
-			: process.argv.slice(2)[0]?.toUpperCase(),
-	});
+const user = new SteamUser({ language: "russian", autoRelogin: false, dataDirectory: null });
 
-	user.once("loggedOn", () => {
-		console.log(`${process.env.ACCOUNT_NAME} logged on!`);
+const accountName = process.env.ACCOUNT_NAME;
+const password = process.env.PASSWORD;
+const twoFactorCode = process.env.SHARED_SECRET.length
+	? SteamTOTP.getAuthCode(process.env.SHARED_SECRET)
+	: process.argv.slice(2)[0]?.toUpperCase();
 
-		user.on("error", err => console.error(err));
-		user.on("disconnected", (result, msg) => console.error(`Disconnected : [EResult: ${result}]\n ${msg}`));
+user.logOn({ accountName, password, twoFactorCode });
 
-		user.setPersona(7); // Invisible
-		user.gamesPlayed(games);
-	});
-})();
+user.on("loggedOn", () => {
+	console.log(`Logged into Steam as ${process.env.ACCOUNT_NAME} [${user.steamID?.getSteamID64()}]`);
+
+	user.on("error", err => console.error(err));
+	user.on("disconnected", (result, msg) => console.error(`Disconnected : [EResult: ${result}]\n ${msg}`));
+
+	user.setPersona(7); // Invisible
+	user.gamesPlayed(games);
+});
