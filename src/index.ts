ff45626f79de09227a661050426a3f30c38a2390
MasedMSD <68379695+MasedMSD@users.noreply.github.com>
2025-03-06 14:47:02 +0500

    little changes

--- src/index.ts
+++ src/index.ts
@@ -10,17 +10,18 @@ import SteamUser from "steam-user";
 import games from "./games.json" with { type: "json" };
 
 const user = new SteamUser({ language: "russian", autoRelogin: false, dataDirectory: null });
+const { ACCOUNT_NAME, PASSWORD, SHARED_SECRET } = process.env;
 
-const accountName = process.env.ACCOUNT_NAME;
-const password = process.env.PASSWORD;
-const twoFactorCode = process.env.SHARED_SECRET.length
-	? SteamTOTP.getAuthCode(process.env.SHARED_SECRET)
+const accountName = ACCOUNT_NAME;
+const password = PASSWORD;
+const twoFactorCode = SHARED_SECRET.length
+	? SteamTOTP.getAuthCode(SHARED_SECRET)
 	: process.argv.slice(2)[0]?.toUpperCase();
 
 user.logOn({ accountName, password, twoFactorCode });
 
 user.on("loggedOn", () => {
-	console.log(`Logged into Steam as ${process.env.ACCOUNT_NAME} [${user.steamID?.getSteamID64()}]`);
+	console.log(`Logged into Steam as ${ACCOUNT_NAME} [${user.steamID?.getSteamID64()}]`);
 
 	user.on("error", err => console.error(err));
 	user.on("disconnected", (result, msg) => console.error(`Disconnected : [EResult: ${result}]\n ${msg}`));
